bd1dfbc2b4f06bb0ecc090f69ca294a3c347d868
Ioana Hanus <ioana.hanus@gmail.com>
2023-12-19 01:44:46 +0200

    added starting menu

---
+++ WitchingTales/Assets/__Assets/Scripts/MenuManagement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManagement : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("TownScene");
+    }
+}
