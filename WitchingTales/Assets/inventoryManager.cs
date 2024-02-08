f89911945d783fd25142ec3007ff5db789123fc1
Starpuff <ioana.hanus1@gmail.com>
2024-02-08 18:10:03 +0200

    ui start

---
+++ WitchingTales/Assets/inventoryManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventoryManager : MonoBehaviour
+{
+    public int objectCount = 0;
+    
+    //public
+    //public void addObject(GameObject object, int objectPosition)
+    //{
+
+    //}
+}
